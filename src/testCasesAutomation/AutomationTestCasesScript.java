bcc0150d9b5f1388c8fd478bc4a9c6f1a29583bc
Anush <anushjaggia@gmail.com>
2023-09-26 15:21:57 +0530

    automation

--- src/testCasesAutomation/AutomationTestCasesScript.java
+++ src/testCasesAutomation/AutomationTestCasesScript.java
@@ -7,13 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AutomationTestCasesScript {
 
 	public static void main(String[] args) {
+		//login with valid credentials 
     WebDriver driver = new ChromeDriver();
     driver.get("https://app.vwo.com/#/login");
     System.out.println( driver.findElement(By.cssSelector("h5#js-sign-in-heading")).getText() ) ;
-    driver.findElement(By.id("login-username")).sendKeys("anushjaggia@gmail.com");
-    driver.findElement(By.name("password")).sendKeys("34782Anush$");
+    driver.findElement(By.id("login-username")).sendKeys("abc@gmail.com");
+    driver.findElement(By.name("password")).sendKeys("34fga2dsch$");
+    driver.findElement(By.cssSelector("use[*|href*='checkbox-button']")).click();
     driver.findElement(By.xpath("//button[@onclick='login.login(event)']")).click();
-  //  driver.close();
+    driver.close();
 	}
 
 }
